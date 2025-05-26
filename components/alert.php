7cd970d6e51e9d2aee574f73964f91d729b2f46e
Nicolas <nicolasmacrez999@gmail.com>
2025-05-26 09:41:17 +0200

    add: index.php, alert.php

---
+++ components/alert.php
@@ -0,0 +1,3 @@
+<div class="h-5 w-full bg-gray-900">
+
+</div>
--- index.php
+++ index.php
@@ -1,1 +1,16 @@
-html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<header>
+    <?php include 'components/header.php'; ?>
+</header>
+
+<body>
+    <?php include 'components/alert.php'; ?>
+</body>
+</html>
