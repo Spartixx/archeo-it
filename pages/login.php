c186fed84d0e14463528a5a6df0f3aedf99a4a48
Nicolas <nicolasmacrez999@gmail.com>
2025-05-26 15:15:25 +0200

    add: login.php, userSQL, connectionSQL

---
+++ pages/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <script src="../assets/js/alert.js"></script>
+
+</head>
+
+<header>
+    <?php include("../components/header.php"); ?>
+</header>
+
+<body class="w-full h-screen max-h-screen bg-white">
+
+<?php include '../components/alert.php'; ?>
+
+<?php
+
+    session_start();
+
+?>
+
+   <div class="md:m-[5rem] lg:m-[10rem] m-[2rem] w-auto h-git border-2 rounded-2xl">
+
+       <img src="" alt="">
+
+   </div>
+
+</body>
+
+</html>
