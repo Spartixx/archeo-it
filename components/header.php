80a72a264c468a3525f03617f5a65720d749aba0
Lucas <lucas.fache0@email.com>
2025-05-26 12:40:29 +0200

    add: modifications du header

--- components/header.php
+++ components/header.php
@@ -6,7 +6,7 @@
 
             <div class="flex items-center">
                 <img
-                        class="h-14 w-14 rounded-tl-[2rem] rounded-tr-[1rem] rounded-br-[2rem] rounded-bl-[1rem] object-cover"
+                        class="h-16 w-16  rounded-tl-[2rem] rounded-tr-[1rem] rounded-br-[2rem] rounded-bl-[1rem] object-cover"
                         src="https://media.discordapp.net/attachments/1304183457022349314/1361249392048210010/raw.png?ex=683570c9&is=68341f49&hm=446a61cb65370adf0e23ad3307e3c044a2fac58f89c8ee95249ab9d32fe76c15&=&format=webp&quality=lossless&width=936&height=936"
                         alt="Logo">
             </div>
@@ -14,13 +14,12 @@
 
 
             <div class="absolute inset-x-0 text-center">
-                <h1 class="text-white text-xl font-bold">Archeo-it</h1>
+                <h1 class="text-white text-4xl font-bold">Archeo-it</h1>
             </div>
 
             <div class="flex items-center pr-[1rem]">
                 <button type="button" class="relative flex rounded-full text-sm ">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="h-14 w-14 rounded-tl-[1rem] rounded-tr-[2rem] rounded-br-[1rem] rounded-bl-[2rem] " src="https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_1280.png" alt="Avatar">
+                    <img class="h-16 w-16 rounded-tl-[1rem] rounded-tr-[2rem] rounded-br-[1rem] rounded-bl-[2rem] " src="https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_1280.png" alt="Avatar">
                 </button>
             </div>
 
