432e4096f8a5c00eab457d59bb392d1a087d5236
Lucas <lucas.fache0@email.com>
2025-05-27 10:42:56 +0200

    add: ajout des href

--- components/header.php
+++ components/header.php
@@ -5,11 +5,15 @@
 
 
             <div class="flex items-center">
-                <img
-                        class="h-16 w-16  rounded-tl-[2rem] rounded-tr-[1rem] rounded-br-[2rem] rounded-bl-[1rem] object-cover"
-                        src="/assets/img/logo-archeo-it.png"
-                        alt="Logo">
+                <a href="/pages/home.php" class="block">
+                    <img
+                            class="h-16 w-16 rounded-tl-[2rem] rounded-tr-[1rem] rounded-br-[2rem] rounded-bl-[1rem] object-cover"
+                            src="/assets/img/logo-archeo-it.png"
+                            alt="Logo"
+                    >
+                </a>
             </div>
+
 
 
 
@@ -19,7 +23,9 @@
 
             <div class="flex items-center pr-[1rem]">
                 <button type="button" class="relative flex rounded-full text-sm ">
+                    <a href="/pages/login.php">
                     <img class="h-16 w-16 rounded-tl-[1rem] rounded-tr-[2rem] rounded-br-[1rem] rounded-bl-[2rem] " src="https://cdn.pixabay.com/photo/2017/11/10/05/48/user-2935527_1280.png" alt="Avatar">
+                    </a>
                 </button>
             </div>
 
