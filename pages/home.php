dea28a1d2151654c966f23566e51dfbaa00de63c
Nicolas <nicolasmacrez999@gmail.com>
2025-06-15 08:40:56 +0200

    edit: home design detail

--- pages/home.php
+++ pages/home.php
@@ -58,11 +58,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     </div>
 
     <div class="w-full h-fit flex flex-col justify-start items-center gap-10 h-100 rounded-xl border-1">
-        <div class="w-full flex flex-col gap-5 justify-start items-center ">
+        <div class="w-full flex flex-col gap-5 justify-start items-center border-xl">
 
             <form method="post" class="w-full h-20 rounded-xl flex flex-row justify-between">
 
-                <button name="BlogSelection" class="flex flex-row items-center justify-center rounded-b-3xl rounded-tr-[0] border-1 border-t-0 border-l-0 sm:text-sm md:text-lg p-2 w-full bg-yellow-950/40 rounded-lg text-black transition hover:bg-yellow-950 hover:text-white">
+                <button name="BlogSelection" class="flex flex-row items-center justify-center rounded-b-3xl rounded-tr-[0] rounded-tl-xl border-1 border-t-0 sm:text-sm md:text-lg p-2 w-full bg-yellow-950/40 text-black transition hover:bg-yellow-950 hover:text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2 h-5 w-5">
                         <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z"></path>
                         <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"></path>
@@ -71,7 +71,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 </button>
 
 
-                <button name="SiteSelection" type="submit" class="flex flex-row items-center justify-center rounded-b-3xl rounded-tl-[0] border-1 border-t-0 border-l-0 border-r-0 sm:text-sm md:text-lg p-2 w-full bg-yellow-950/40 rounded-lg text-black transition hover:bg-yellow-950 hover:text-white">
+                <button name="SiteSelection" type="submit" class="flex flex-row items-center justify-center rounded-b-3xl rounded-tl-[0] rounded-tr-xl  border-1 border-t-0 border-x-0 border-r-0 sm:text-sm md:text-lg p-2 w-full bg-yellow-950/40 text-black transition hover:bg-yellow-950 hover:text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mr-2 h-5 w-5">
                         <line x1="3" x2="21" y1="22" y2="22"></line>
                         <line x1="6" x2="6" y1="18" y2="11"></line>
